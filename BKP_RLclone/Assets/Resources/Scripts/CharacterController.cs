f73fea8c6601d07292c2bc375ca166d00d9adada
Brendan <bkpratt93@gmail.com>
2020-07-28 00:10:29 -0700

    Added drone!

---
+++ BKP_RLclone/Assets/Resources/Scripts/CharacterController.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+
+public class CharacterController : MonoBehaviour
+{
+    public float Speed = 5;
+
+    public void MoveForward()
+    {
+        transform.Translate(transform.forward * Time.deltaTime * Speed);
+    }
+
+    public void MoveBackward()
+    {
+        transform.Translate(-transform.forward * Time.deltaTime * Speed);
+    }
+
+    public void MoveRight()
+    {
+        transform.Translate(transform.right * Time.deltaTime * Speed);
+    }
+
+    public void MoveLeft()
+    {
+        transform.Translate(-transform.right * Time.deltaTime * Speed);
+    }
+
+    public void MoveUp()
+    {
+        transform.Translate(transform.up * Time.deltaTime * Speed);
+    }
+
+    public void MoveDown()
+    {
+        transform.Translate(-transform.up * Time.deltaTime * Speed);
+    }
+
+}
---
+++ BKP_RLclone/Assets/Resources/Scripts/PlayerInput.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+
+public class PlayerInput : MonoBehaviour
+{
+    public CharacterController CharacterController;
+
+    private void Update()
+    {
+        if (Input.GetKey(KeyCode.UpArrow) || Input.GetKey(KeyCode.W))
+        {
+            CharacterController.MoveForward();
+        }
+        if (Input.GetKey(KeyCode.DownArrow) || Input.GetKey(KeyCode.S))
+        {
+            CharacterController.MoveBackward();
+        }
+        if (Input.GetKey(KeyCode.LeftArrow) || Input.GetKey(KeyCode.A))
+        {
+            CharacterController.MoveLeft();
+        }
+        if (Input.GetKey(KeyCode.RightArrow) || Input.GetKey(KeyCode.D))
+        {
+            CharacterController.MoveRight();
+        }
+        if (Input.GetKey(KeyCode.LeftShift))
+        {
+            CharacterController.MoveUp();
+        }
+        if (Input.GetKey(KeyCode.C))
+        {
+            CharacterController.MoveDown();
+        }
+
+    }
+}
